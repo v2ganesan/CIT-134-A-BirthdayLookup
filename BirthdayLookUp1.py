89436400bb0e9cd38d9c47a933f172801f924182
v2ganesan <varun.mg554@gmail.com>
2021-03-18 19:01:35 -0700

    Added a comment

--- BirthdayLookUp1.py
+++ BirthdayLookUp1.py
@@ -1,6 +1,25 @@
 import json
 
-with open('birthday.json') as jsonFile:
-    dict = json.loads(jsonFile)
+try:
+    jsonFile = open('birthday.json', 'r')
+except OSError:
+    print("ERROR: Unable to open the file.")
 
-print(type(dict))
+birthdayList = json.load(jsonFile)
+birthdayDictionary = {} 
+
+#creates empty dictionary to put values into 
+for elem in birthdayList:
+    name = elem["name"]
+    birthday = elem["birthday"]
+
+    birthdayDictionary[name] = birthday
+
+name = input("Enter a name: ")
+print("Name: " + name)
+
+
+if name in birthdayDictionary.keys():
+    print("Birthday: " + birthdayDictionary[name])
+else:
+    print("ERROR!")
