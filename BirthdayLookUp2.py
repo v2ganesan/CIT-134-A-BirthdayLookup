c7f3da3c057f20411bb56aa2c1834e51da58a4b5
v2ganesan <72575838+v2ganesan@users.noreply.github.com>
2021-04-01 23:17:35 -0700

    Add files via upload

---
+++ BirthdayLookUp2.py
@@ -0,0 +1,34 @@
+import json
+
+try:
+    jsonFile = open('birthday.json', 'r')
+except OSError:
+    print("ERROR: Unable to open the file.")
+
+birthdayList = json.load(jsonFile)
+birthdayDictionary = {} 
+
+#creates empty dictionary to put values into 
+for elem in birthdayList:
+    name = elem["name"]
+    birthday = elem["birthday"]
+
+    birthdayDictionary[name] = birthday
+
+
+while True:
+    search = str(input("Enter a name: "))
+
+    birthdays = []
+    if search != "Exit":
+        for key in birthdayDictionary.keys():
+            if search in key:
+                print(key)
+                birthdays.append(birthdayDictionary[key])
+            else:
+                print("ERROR")
+                break
+        print(birthdays)
+    else:
+        print("Done.")
+
